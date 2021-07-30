47e46cc8c358e12d0f2c678e3944087d524abb24
Htoo Maung Thait <htoomaungthait@gmail.com>
2021-07-31 00:04:53 +0630

    pos header controls panel

--- resources/views/sale/main-sale-page.blade.php
+++ resources/views/sale/main-sale-page.blade.php
@@ -1,28 +1,90 @@
 @extends('layouts.master')
 
-@section('title', session('page_title'));
-
+@section('title', session('page_title'))
 
 @section('content')
-     <!-- Content Header (Page header) -->
-     <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">{{session('page_title')}}</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item">{{session('page_title')}}</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
+    {{-- Content --}}
+    <div>
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">{{ session('page_title') }}</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item active">{{ session('page_title') }}</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+                {{-- header action --}}
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+
+
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class=" col-md-4">
+                                            <button class="btn btn-outline-primary">
+                                                <i class="fa fa-plus-circle"></i> &nbsp;
+                                                New Vouncher
+                                            </button>
+
+                                            <button class="btn btn-outline-danger">
+                                                <i class="fa fa-trash"></i> &nbsp;
+                                                Remove Vouncher
+                                            </button>
+                                        </div>
+                                        <label for="voucherTotal" class="col-md-1 form-label col-form-label-md mt-1">Total
+                                            :</label>
+                                        <div class="col-md-3">
+                                            <input type="text" class="form-control" readonly value="400,000" />
+                                        </div>
+
+                                        <div class="col-md-4">
+                                            <h4> pagination </h4>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+                {{-- header action end --}}
+
+                {{-- item add/edit --}}
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card pos-item-card">
+                            <div class="card-body">
+                                <div class="container-fluid">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- item add/edit end --}}
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
     </div>
-    <!-- /.content-header -->
+    {{-- /Content --}}
+
 @endsection
-
-
-@push('page_js_script')
-
-@endpush
